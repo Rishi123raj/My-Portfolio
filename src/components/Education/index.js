569587ef208272b3574ffdabdb166c6801992875
Rishi123raj <221220051@nitdelhi.ac.in>
2024-07-27 10:57:56 +0530

    changes are made

--- src/components/Education/index.js
+++ src/components/Education/index.js
@@ -92,10 +92,7 @@ const index = () => {
                                 <TimelineContent sx={{ py: '12px', px: 2 }}>
                                     <EducationCard education={education}/>
                                 </TimelineContent>
-                                <TimelineSeparator>
-                                    <TimelineDot variant="outlined" color="secondary" />
-                                    {index !== experiences.length  && <TimelineConnector style={{ background: '#854CE6' }} />}
-                                </TimelineSeparator>
+                                
                             </TimelineItem>
                         ))}
                     </Timeline>
